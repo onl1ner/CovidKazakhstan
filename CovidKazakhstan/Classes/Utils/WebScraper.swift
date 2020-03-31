16c9932228a82b93d3169135cdecc96a9d6d462e
onl1ner <netv0idrug@mail.ru>
2020-03-31 23:13:56 +0600

    Маленькие правки

--- CovidKazakhstan/Classes/Utils/WebScraper.swift
+++ CovidKazakhstan/Classes/Utils/WebScraper.swift
@@ -15,42 +15,38 @@ final class WebScrapper {
     private let data : HTMLDocument
     
     private func getInfectedByCity() -> [String : Int] {
-        var infected = data.xpath("//div[@class=\"city_cov\"]")[0].text?.deleteAllSpaces()
+        let infected = data.xpath("//div[@class=\"city_cov\"]")[0].text?.deleteAllSpaces()
         
         var infectedDict : [String : Int] = [:]
-        /// При скрапинге данных получаем на выходе скотское форматирование, где перед первым городом получаем перенос строки(\n)
-        /// и после последнего элемента два переноса на новую строку. Две следующие строки убирают это дело
-        infected?.removeFirst(1)
-        infected?.removeLast(2)
         
         for i in (infected?.components(separatedBy: .newlines))! {
-            let infectedAmount = Int(i.components(separatedBy: characterSet).joined())!
-            
-            var infectedCity = i.components(separatedBy: .decimalDigits).joined()
-            infectedCity.removeLast()
-            
-            infectedDict[infectedCity] = infectedAmount
+            if i.count > 0 {
+                let infectedAmount = Int(i.components(separatedBy: characterSet).joined())!
+
+                var infectedCity = i.components(separatedBy: .decimalDigits).joined()
+                infectedCity.removeLast()
+
+                infectedDict[infectedCity] = infectedAmount
+            }
         }
         
         return infectedDict
     }
     
     private func getRecoveredByCity() -> [String : Int] {
-        var recovered = data.xpath("//div[@class=\"city_cov\"]")[1].text?.deleteAllSpaces()
+        let recovered = data.xpath("//div[@class=\"city_cov\"]")[1].text?.deleteAllSpaces()
         
         var recoveredDict : [String : Int] = [:]
-        /// При скрапинге данных получаем на выходе скотское форматирование, где перед первым городом получаем перенос строки(\n)
-        /// и после последнего элемента один перенос на новую строку. Две следующие строки убирают это дело
-        recovered?.removeFirst(1)
-        recovered?.removeLast(1)
         
-        for rec in (recovered?.components(separatedBy: .newlines))! {
-            let recoveredAmount = Int(rec.components(separatedBy: characterSet).joined())
-            
-            var recoveredCity = rec.components(separatedBy: .decimalDigits).joined()
-            recoveredCity.removeLast()
-            
-            recoveredDict[recoveredCity] = recoveredAmount
+        for i in (recovered?.components(separatedBy: .newlines))! {
+            if i.count > 0 {
+                let recoveredAmount = Int(i.components(separatedBy: characterSet).joined())
+                
+                var recoveredCity = i.components(separatedBy: .decimalDigits).joined()
+                recoveredCity.removeLast()
+                
+                recoveredDict[recoveredCity] = recoveredAmount
+            }
         }
         
         return recoveredDict
